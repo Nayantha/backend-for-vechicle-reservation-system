08b6ecb2405fa8a976f3db37cafe36f64bb6cd44
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-19 20:26:06 +0530

    Initial Commit.

---
+++ src/main/java/com/ny_vrs/backend/VehicleReservationServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ny_vrs.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleReservationServiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleReservationServiceBackendApplication.class, args);
+    }
+
+}
