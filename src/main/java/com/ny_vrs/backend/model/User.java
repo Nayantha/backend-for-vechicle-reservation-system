9b1154e204e6dba2511652762bb210aa4453b64b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-19 21:19:40 +0530

    add some of the variables needed to construct user entity.

--- src/main/java/com/ny_vrs/backend/model/User.java
+++ src/main/java/com/ny_vrs/backend/model/User.java
@@ -1,4 +1,21 @@
 package com.ny_vrs.backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+    private String email;
+    private String phoneNumber;
+
 }
