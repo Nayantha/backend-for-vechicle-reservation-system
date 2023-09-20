43f0805e7a8d31c920238efaf4e8559c89f87f67
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-20 10:44:08 +0530

    add a method and the rest of the annotations needed.

--- src/main/java/com/ny_vrs/backend/service/UserService.java
+++ src/main/java/com/ny_vrs/backend/service/UserService.java
@@ -1,4 +1,17 @@
 package com.ny_vrs.backend.service;
 
+import com.ny_vrs.backend.exception.UserNotFoundException;
+import com.ny_vrs.backend.model.User;
+import com.ny_vrs.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public final User findUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("User not exists with that email."));
+    }
 }
