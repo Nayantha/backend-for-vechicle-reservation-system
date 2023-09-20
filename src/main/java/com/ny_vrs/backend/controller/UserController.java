98529420d27341ab628edfae3a93fd2ee5bb1725
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-20 10:51:48 +0530

    add a method to get user by email.

--- src/main/java/com/ny_vrs/backend/controller/UserController.java
+++ src/main/java/com/ny_vrs/backend/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.ny_vrs.backend.controller;
 
+import com.ny_vrs.backend.model.User;
+import com.ny_vrs.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/get_from_email")
+    public User getUserFromEmail(@RequestBody String email) {
+        return userService.findUserByEmail(email);
+    }
 }
